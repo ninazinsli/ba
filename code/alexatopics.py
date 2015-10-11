973d6290514603be04502a69964f6fdc39cb76a5
root <root@dco-node138.dco.ethz.ch>
2015-10-11 18:42:29 +0200

    alexatopic modified

--- code/alexatopics.py
+++ code/alexatopics.py
@@ -12,65 +12,78 @@ def closeconnection(cur):
     cur.close()
     conn.close()
 
-def sitename(site):
-    title = ""    
+def get_sitename(site):
+    site_name = ""    
     soup = BeautifulSoup(site, 'html.parser')
-    title = soup.title.name.string
-    title = title.split(" ")[0]
-    try:
-        title2 = site[site.find("<title>")+7:site.find("Site Overview")-1]
-    print("Title soup: " + title)
-    print("Title: " + title2)
-    return title
+    site_name = soup.title.string
+    #print("Site_name: " + site_name)
+    site_name = site_name.split(" ")[0]
+    #print("Sitename: " + site_name + ".")
+    return site_name
     
 def gettopic(site):
-    topic = ""
+    topic = []
     soup = BeautifulSoup(site, 'html.parser')
-    print("Topic: " + topic)
+    table = soup.find("table", {"id":"category_link_table"})
+    for row in table.findAll("td"):
+        t = ""
+        for a in row.findAll('a'):            
+            t += a.get_text()
+            t += " / "
+        topic.append(t)
+
+    if "" in topic and len(topic) == 1:
+        topic.append("no category")
+        topic.remove("")
+    print("Topic: " + str(topic))
     return topic
     
     
 def addtopic(site, domains):
     cur = connect()
     cur2 = connect()
-    sitename = sitename(site)
+    sitename = get_sitename(site)
     # If information about one of our sites, get topic information
-    if 'sitename' in domains:
-        topic = gettopic(site)
-        topicquery = "SELECT id FROM categoriystring \
-                      WHERE categories like \'%s\'" %topic
-        cur.execute(topicquery)
-        # If topic already in database, add info about site to database
-        # If not, first add topic to database
-        if not cur.fetchone():
-            topicinsert = "INSERT INTO categorystrings (categories) \
-                           values(\'%s\')" %topic
-            cur2.execute(topicinsert)
-            topicquery = "SELECT id FROM categoriystring \
+    if sitename in domains:
+        #print("sitename found: " + sitename)
+        topics = gettopic(site)
+        for topic in topics:
+            topicquery = "SELECT id FROM categorystrings \
                           WHERE categories like \'%s\'" %topic
             cur.execute(topicquery)
-        id = cur.fetchone()[0]
-        insertsite = "INSERT INTO sites (name, categorystring) \
-                      VALUES(\'%s\',\'%s\')" %(sitename, id)
-        cur2.execute(insertsite)
+            # If topic already in database, add info about site to database
+            # If not, first add topic to database
+            if not cur.fetchone():
+                topicinsert = "INSERT INTO categorystrings (categories) \
+                               values(\'%s\')" %topic
+                cur2.execute(topicinsert)
+                topicquery = "SELECT id FROM categorystrings \
+                              WHERE categories like \'%s\'" %topic
+                cur.execute(topicquery)
+            id = cur.fetchone()[0]
+            insertsite = "INSERT INTO sites (name, categorystring) \
+                          VALUES(\'%s\',\'%s\')" %(sitename, id)
+            cur2.execute(insertsite)
         
         
     closeconnection(cur)
     closeconnection(cur2)
+
+    
         
 def main():
     cur = connect()
 
     # get all domains and safe them alphabetically ordered in a list
     sql_domain = "SELECT distinct(domain) FROM events \
-                  WHERE domain is not null \
-                  LIMIT 10"
+                  WHERE domain is not null"
     
     cur.execute(sql_domain)
     #domains = list(cur.fetchall())[6:] # first 6 elements are not usable
     domains = set(cur.fetchall())
     #print(domains)
-
+    print("Domains geholt: " + len(domains))
+    #domains.add("007james.com")
 
     # Read alexa file info (one site at a time)
     alexafile = open("../../alexa/alexa-site-info")
